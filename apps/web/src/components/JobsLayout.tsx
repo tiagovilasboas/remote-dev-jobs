f8474019de6590eec7ee0f6208eec6c978809e7c
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-08 00:20:27 -0300

    style(ui): widen list column to improve card readability

--- apps/web/src/components/JobsLayout.tsx
+++ apps/web/src/components/JobsLayout.tsx
@@ -8,10 +8,10 @@ interface Props {
 export default function JobsLayout({ list, detail }: Props) {
   return (
     <div className="grid min-h-screen grid-cols-12 gap-4 p-4 bg-gray-50">
-      <div className="col-span-12 md:col-span-4 overflow-y-auto border-r border-gray-200 pr-4">
+      <div className="col-span-12 md:col-span-5 overflow-y-auto border-r border-gray-200 pr-4">
         {list}
       </div>
-      <div className="col-span-12 md:col-span-8 overflow-y-auto pl-4">
+      <div className="col-span-12 md:col-span-7 overflow-y-auto pl-4">
         {detail}
       </div>
     </div>
